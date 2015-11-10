4ff2288a1b27d13fa455a004a93037012dfcd053
spam@alancowap.com <wahler55!!>
2015-11-10 15:50:07 +0000

    I/O, For loop

--- ICT-AP_Console/Program.cs
+++ ICT-AP_Console/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/** 
+ *  Simple class to demo C#
+ *  Basic input and output, for loop
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ */
+
+using System;
 
 namespace ICTAP_Console
 {
@@ -6,7 +16,28 @@ namespace ICTAP_Console
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			// prompt user for name
+			Console.WriteLine ("Please enter your name ");
+
+			// store users name
+			string name = Console.ReadLine ();
+
+			// Get number of times to print name
+			Console.WriteLine ("How many times... ");
+			string numTimes = Console.ReadLine();
+			int number;
+			Int32.TryParse(numTimes, out number);
+			++number;
+
+			// print users name 
+			for (int i = 1; i < number; i++)
+			{
+				Console.WriteLine (i + "Your name is " + name);
+			}
+
+			//hack to keep command window open
+			Console.WriteLine ("\n\nHit RETURN to Exit");
+			Console.Read ();
 		}
 	}
 }
