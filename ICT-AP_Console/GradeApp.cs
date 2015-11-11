443f7ef5c5ea7e91374181126cf0eb8dbfde1b68
spam@alancowap.com <wahler55!!>
2015-11-11 14:16:35 +0000

    GradeApp - demo if, do-while,

---
+++ ICT-AP_Console/GradeApp.cs
@@ -0,0 +1,77 @@
+﻿/** 
+ *  Simple class to demo if statement
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ */
+
+using System;
+
+namespace ICTAP_Console
+{
+	public class GradeApp
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine ("Welcome to GradeApp");
+
+			// get user input
+			bool isInvalidInput = false;
+			int score = -1;
+			do{
+				isInvalidInput = false;
+				Console.WriteLine ("Please enter a score in the range 0..100 inclusive:");
+				string userChoice = Console.ReadLine ();
+				bool isValidType = Int32.TryParse (userChoice, out score); //Type Check
+				isInvalidInput = !isValidType;
+				// Console.WriteLine ("You chose " + score);
+
+				//validate user input is 0..100 Range Check
+				if(score > 100 || score < 0)
+				{
+					isInvalidInput = true;
+					Console.WriteLine ("Score is invalid");
+				}
+			}while(isInvalidInput);
+
+
+			//calculate grade
+			string grade = "Unknown";
+			if(score > 84)
+			{
+				grade = "A";
+			}
+			else if(score > 69)
+			{
+				grade = "B";
+			}
+			else if(score > 54)
+			{
+				grade = "C";
+			}
+			else if(score > 39)
+			{
+				grade = "D";
+			}
+			else
+			{
+				grade = "Fail";
+			}
+				
+				
+
+
+			
+			//output result
+			Console.WriteLine("Your grade is: " + grade);
+
+			//hack to keep command window open
+			Console.WriteLine ("\n\nHit RETURN to Exit");
+			Console.Read ();
+
+		}
+	}
+}
+
--- ICT-AP_Console/Program.cs
+++ ICT-AP_Console/Program.cs
@@ -14,8 +14,9 @@ namespace ICTAP_Console
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		public static void Main2 (string[] args)
 		{
+
 			// prompt user for name
 			Console.WriteLine ("Please enter your name ");
 
@@ -23,21 +24,39 @@ namespace ICTAP_Console
 			string name = Console.ReadLine ();
 
 			// Get number of times to print name
-			Console.WriteLine ("How many times... ");
+			Console.WriteLine ("How many times do you want your name printed? ");
 			string numTimes = Console.ReadLine();
 			int number;
 			Int32.TryParse(numTimes, out number);
 			++number;
 
+			//Conditional statement - if
+			if (number > 11)
+			{
+				Console.WriteLine ("That's a lot of times!");
+			}
+			else if (number < 6)
+			{
+				Console.WriteLine ("That's not many times");
+			}
+			else
+			{
+				Console.WriteLine ("That's about right");	
+			}
+
 			// print users name 
 			for (int i = 1; i < number; i++)
 			{
-				Console.WriteLine (i + "Your name is " + name);
+				Console.WriteLine (i + " Your name is " + name);
 			}
+
 
 			//hack to keep command window open
 			Console.WriteLine ("\n\nHit RETURN to Exit");
 			Console.Read ();
 		}
+
+
+
 	}
 }
