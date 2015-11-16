0ad08fb547706c14219a8eb238b3511270d4939a
spam@alancowap.com <wahler55!!>
2015-11-16 12:10:20 +0000

    Add ArrayList

--- ICT-AP_Console/MethodApp.cs
+++ ICT-AP_Console/MethodApp.cs
@@ -1,5 +1,5 @@
 ﻿// /** 
-//  *  Class to demo ...
+//  *  Class to demo method declarations, parameters, return types, and ArrayList
 //  *  
 //  * @author Alan Cowap 
 //  * @version 1.0  
@@ -8,6 +8,7 @@
 //  */
 //
 using System;
+using System.Collections;
 
 namespace ICTAP_Console
 {
@@ -17,24 +18,44 @@ namespace ICTAP_Console
 		{
 			Console.WriteLine ("Welcome to MethodApp");
 			int[] data = {1,2,3,4,5};
-			int min = CalculateMin (data);
-			Console.WriteLine ("Min is {0} ", min);
 
+			int min = CalculateMin (data);
+			int max = CalculateMax (data);
 			DisplayNumbers (data);
+			Console.WriteLine ("Min is {0}, Max is {1} ", min, max);
+			data [1] = -8;
 			data [3] = 10;
+
 			min = CalculateMin (data);
+			max = CalculateMax (data);
 			DisplayNumbers (data);
-			Console.WriteLine ("Min is {0} ", min);
+			Console.WriteLine ("Min is {0}, Max is {1} ", min, max);
+
+			//ArrayList example
+			ArrayList al = new ArrayList();
+			Console.WriteLine ("\nArrayList size is {0}", al.Count);
+			al.Add ("This");
+			Console.WriteLine ("ArrayList size is {0}", al.Count);
+			al.RemoveAt (0);
+			Console.WriteLine ("ArrayList size is {0}", al.Count);
+			al.Add ("This");
+			al.Add ("That");
+			al.Add ("Other");
+			Console.WriteLine ("ArrayList size is {0}", al.Count);
+
+			foreach(string str in al)
+			{
+				Console.Write (str + " ");
+			}
 
 			Console.Write ("\nPress RETURN to continue");
 			Console.Read ();
-
 		}
 
 		public static void DisplayNumbers(int[] numbers)
 		{
-			//display 5 numbers to user
-			for (int i = 0; i < 5; ++i) 
+			//display numbers to user
+			for (int i = 0; i < numbers.Length; ++i) 
 			{
 				Console.WriteLine (numbers[i]);
 			}
@@ -42,9 +63,9 @@ namespace ICTAP_Console
 
 		public static int CalculateMin(int[] numbers)
 		{
-			Console.WriteLine ("You are in calculateMin");
+			//Console.WriteLine ("You are in calculateMin");
 			int min = numbers[0];
-			for (int i = 1; i < 5; ++i) 
+			for (int i = 1; i < numbers.Length; ++i) 
 			{
 				if(numbers[i] < min)
 				{
@@ -53,6 +74,21 @@ namespace ICTAP_Console
 			}
 			return min;
 		}
+
+		public static int CalculateMax(int[] numbers)
+		{
+			//Console.WriteLine ("You are in calculateMin");
+			int max = numbers[0];
+			for (int i = 1; i < numbers.Length; ++i) 
+			{
+				if(numbers[i] > max)
+				{
+					max = numbers[i];
+				}
+			}
+			return max;
+		}
+
 
 	}
 }
